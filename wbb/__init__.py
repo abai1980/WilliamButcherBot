87ca418ef31239d04a166785a5b356774ffa0da6
abai1980 <88306838+abai1980@users.noreply.github.com>
2021-09-05 16:35:50 +0530

    Update __init__.py

--- wbb/__init__.py
+++ wbb/__init__.py
@@ -33,7 +33,6 @@ from pyrogram.types import Message
 from pyromod import listen
 from Python_ARQ import ARQ
 from motor import motor_asyncio
-from odmantic import AIOEngine
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 
@@ -56,11 +55,8 @@ bot_start_time = time.time()
 
 # MongoDB client
 print("[INFO]: INITIALIZING DATABASE")
-mongodb = MongoClient(MONGO_URI, MONGO_PORT)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_URI, MONGO_PORT)
-db = motor[MONGO_DB]
-
-engine = AIOEngine(motor, MONGO_DB)
+mongo_client = MongoClient(MONGO_URI, MONGO_PORT)
+db = mongo_clinet.wbb
 
 async def load_sudoers():
     global SUDOERS
